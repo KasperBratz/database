de8b96b0f7e0c08a1251e9ed90978aa6444d52ca
Philip <pedeersen@hotmail.com>
2016-03-22 10:29:15 +0100

    initial.sql gjord

--- initial.sql
+++ initial.sql
@@ -18,9 +18,10 @@ create table Orders(
 	deliveryTime date,
 	customerName varChar(255),
 	delivered boolean,
-	foreign key(customerName) references Customers(name)
-
+	foreign key (customerName) references Customers(name)
 );
+
+
 create table RawMaterials(
 	name varChar(255) primary key,
 	amount int,
@@ -29,7 +30,7 @@ create table RawMaterials(
 );
 
 create table Cookies(
-	cookieName varChar(255)
+	cookieName varChar(255) primary key
 );
 
 create table Recipe(
@@ -37,8 +38,8 @@ create table Recipe(
 	ingredient varChar(255),
 	amount int,
 	primary key (cookieName,ingredient),
-	foreign key cookieName references Cookies(cookieName),
-	foreign key ingredient references RawMaterials(name)
+	foreign key (cookieName) references Cookies(cookieName),
+	foreign key (ingredient) references RawMaterials(name)
 
 );
 
@@ -49,6 +50,31 @@ create table Pallets(
 	cookieName varChar(255),
 	blocked boolean,
 	sent boolean,
-	foreign key orderNbr references Orders(orderNbr),
-	foreign key cookieName references Cookies(cookieName)
+	foreign key (orderNbr) references Orders(orderNbr),
+	foreign key (cookieName) references Cookies(cookieName)
 );
+
+insert into Customers values("Finkakor AB", "Helsingborg");
+insert into Customers values("Småbröd AB", "Malmö");
+insert into Customers values("Partybröd AB", "Månen");
+
+insert into Cookies values("Amneris");
+insert into Cookies values("Ballerina");
+insert into Cookies values("Tango");
+
+
+insert into RawMaterials values("Flour", 270000, '2016-03-03',10000);
+insert into RawMaterials values("Suger", 20000, '2016-03-03', 600);
+insert into RawMaterials values("Egg", 1000, '2016-03-03',500);
+
+insert into Recipe values("Tango", "Flour", 270);
+insert into Recipe values("Tango", "Suger", 200);
+insert into Recipe values("Tango", "Egg", 10);
+
+insert into Recipe values("Ballerina", "Flour", 200);
+insert into Recipe values("Ballerina", "Suger", 170);
+insert into Recipe values("Ballerina", "Egg", 8);
+
+insert into Recipe values("Amneris", "Flour", 180);
+insert into Recipe values("Amneris", "Suger", 100);
+insert into Recipe values("Amneris", "Egg", 12);
